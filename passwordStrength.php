114c15b6a67a1edf678366f75b7ecd48a4563f8e
Alexander Kozlov <al.iv.kozlov@yandex.ru>
2016-05-25 18:32:21 +0300

    Released Remove Extra Blanks & Password Strength
    Signed-off-by: Alexander Kozlov <al.iv.kozlov@yandex.ru>

---
+++ passwordStrength.php
@@ -0,0 +1,11 @@
+<?php
+	require_once('include\common.inc.php');
+	if (isset($_GET['password']))
+	{
+		$password = $_GET['password'];
+		echo passwordStrength($password); 
+	}
+	else
+	{
+		header("HTTP/1.1 400 Bad string!");
+	}
